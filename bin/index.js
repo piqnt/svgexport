9bb9a9aa44573dab6c5481a8d4d1c7fb8e333898
Oleg Lebedev <oleg@canva.com>
2022-09-13 13:08:59 +1000

    fix the entry point

--- bin/index.js
+++ bin/index.js
@@ -1,2 +1,10 @@
 #!/usr/bin/env node
-require('../').cli(process.argv.slice(2));
+require('../')
+ .cli(process.argv.slice(2))
+ .then(
+  function() {},
+  function(e) {
+    console.error(e);
+    process.exit(1);
+  }
+ );
